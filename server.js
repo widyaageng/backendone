271b021400cb4e0125acde49fedef5939b3f9c47
widyaageng <ageng.setyatutuko@gmail.com>
2021-12-08 00:55:21 +0800

    working unix time

--- server.js
+++ server.js
@@ -4,6 +4,14 @@
 // init project
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+// custom middleware
+// logging rest actions
+function applogger(req, res, next) {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+}
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -13,20 +21,39 @@ app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 20
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
+// middlewares
+app.use(path='/', middlewareFunction=bodyParser.urlencoded({extended: false}));
+app.use(path='/', middlewareFunction=applogger);
+
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (req, res) {
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
+// api router
+app.get('/api/hello', function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get('/api/:date', function (req, res) {
+  let regPat = String(req.params.date).split(/-/);
+  let dateInput = Date.now();
+  if (regPat.length > 1) {
+    dateInput = new Date(parseInt(regPat[0]), parseInt(regPat[1]) - 1, parseInt(regPat[2]));
+  } else if (regPat.match(/[0-9]+/).input === regPat) {
+    dateInput = new Date(parseInt(regPat));
+  } else {
+    //
+  }
+  res.send({
+    unix: dateInput.valueOf(),
+    utc: dateInput.toUTCString()
+  })
+})
 
+//2015-12-25
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
